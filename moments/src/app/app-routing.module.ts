8b14007122e4419bb8e7e8569abd408a4e27d320
LoiolaBr17 <mateustaua16@gmail.com>
2022-11-23 17:18:15 -0300

    criando rotas para o componente newMoment

--- moments/src/app/app-routing.module.ts
+++ moments/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: "about", component: AboutComponent}
+  {path: "about", component: AboutComponent},
+  {path: 'moments/new', component: NewMomentComponent
+}
 ];
 
 @NgModule({
