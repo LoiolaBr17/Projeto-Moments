529acc9941f2469a59eb6aa3af962a277693fce1
LoiolaBr17 <mateustaua16@gmail.com>
2022-11-24 10:52:01 -0300

    criando funcao de post no servico moment

--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Moment } from 'src/app/moment';
 
+import { MomentService } from 'src/app/services/moment.service';
+
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
@@ -10,13 +12,25 @@ import { Moment } from 'src/app/moment';
 export class NewMomentComponent implements OnInit {
   btnText = "Compartilhar"
 
-  constructor() { }
+  constructor(
+    private momentService: MomentService
+  ) { }
 
   ngOnInit(): void {
   }
 
   async createHandler(moment: Moment){
-    console.log("funfou")
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append("description", moment.description)
+    
+    if(moment.image) {
+      formData.append("image", moment.image)
+    }
+
+    await this.momentService.createMoment(formData).subscribe()
+
   }
 
 }
